a7519379cd39e4d96ee3626c1f3a61ffa28d6c3d
brunno <web.brunno@gmail.com>
2018-05-16 17:28:14 -0300

    Cria projeto.

---
+++ poc.risco.project/poc.Risco.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace poc.Risco.api.Controllers {
+
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class AuthenticationController : Controller {
+
+        [HttpGet]
+        public string Get() {
+
+            return System.Environment.OSVersion.VersionString;
+        }
+
+        [HttpPost]
+        public string SignInUser()
+        {
+
+            return null;
+        }
+    }
+}
---
+++ poc.risco.project/poc.Service/AuthenticationService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace poc.Service {
+
+    public sealed class AuthenticationService : AbstractService {
+
+    }
+}
